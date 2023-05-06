eb71caa7f193e9e2b8d3cd11ee859d09ef3a8d3c
kiiigaaa <120770077+kiiigaaa@users.noreply.github.com>
2023-05-06 16:37:32 +0530

    Changed AppDatabase.kt

--- app/src/main/java/com/example/srifin/AppDatabase.kt
+++ app/src/main/java/com/example/srifin/AppDatabase.kt
@@ -6,4 +6,7 @@ import androidx.room.RoomDatabase
 @Database(entities = arrayOf(Transaction::class), version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun transactionDao() : TransactionDao
+
+
+
 }
