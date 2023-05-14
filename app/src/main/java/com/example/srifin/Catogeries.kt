d29117869f99d45f7c7d133e99f2972765e5ec6c
Sadeesha Ashan <120770131+Sadeeshaashan@users.noreply.github.com>
2023-05-14 21:53:20 +0530

    Updated all

---
+++ app/src/main/java/com/example/srifin/Catogeries.kt
@@ -0,0 +1,7 @@
+package com.example.srifin
+
+data class Catogeries(
+    var categorydescription: String? = "",
+    var categoryname: String? = "",
+    val collectionid: String? = " ",
+)
--- app/src/main/java/com/example/srifin/Control.kt
+++ app/src/main/java/com/example/srifin/Control.kt
@@ -1,72 +1,64 @@
 package com.example.srifin
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.FirebaseDatabase
 
 class Control : AppCompatActivity() {
-
-    private lateinit var tvId: TextView
-    private lateinit var tvTit: TextView
-    private lateinit var tvDes: TextView
-    private lateinit var btnUpdate: Button
-    private lateinit var btnDelete: Button
+    private lateinit var etNwsTit: EditText
+    private lateinit var etNwsDes: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_control)
-        initView()
-        setValuesToViews()
 
+        // Initialize views
+        etNwsTit = findViewById(R.id.tvNwsTit)
+        etNwsDes = findViewById(R.id.tvNwsDes)
+        val btnUpdate = findViewById<Button>(R.id.btnUpdate)
+        val btnView = findViewById<Button>(R.id.btnUpdate2)
+
+        // Set click listeners for buttons
         btnUpdate.setOnClickListener {
-            val intent = Intent(this, update::class.java)
-            intent.putExtra("Id", intent.getStringExtra("Id"))
-            intent.putExtra("Title", intent.getStringExtra("Title"))
-            intent.putExtra("Description", intent.getStringExtra("Description"))
+            // Get the values entered by the user
+            val title = etNwsTit.text.toString()
+            val description = etNwsDes.text.toString()
+
+            // Push the updates to Firebase database
+            val database = FirebaseDatabase.getInstance()
+            val myRef = database.reference
+            myRef.child("title").setValue(title)
+            myRef.child("description").setValue(description)
+                .addOnSuccessListener {
+                    // Show success message
+                    Toast.makeText(
+                        this@Control,
+                        "Updated successfully",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    // Clear the EditText fields
+                    etNwsTit.text.clear()
+                    etNwsDes.text.clear()
+                }
+                .addOnFailureListener {
+                    // Show error message
+                    Toast.makeText(
+                        this@Control,
+                        "Failed to update",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+        }
+        btnView.setOnClickListener {
+            // Navigate to Read.kt activity
+            val intent = Intent(this@Control, Read::class.java)
             startActivity(intent)
         }
-        btnDelete.setOnClickListener {
-            deleteRecord(
-                intent.getStringExtra("Id").toString()
-            )
-        }
     }
-
-
-    private fun initView() {
-        tvId = findViewById(R.id.tvNwsId)
-        tvDes = findViewById(R.id.tvNwsDes)
-        tvTit = findViewById(R.id.tvNwsTit)
-
-
-        btnUpdate = findViewById(R.id.btnUpdate)
-        btnDelete = findViewById(R.id.btnDelete)
-    }
-
-    private fun setValuesToViews() {
-        tvId.text = intent.getStringExtra("Id")
-        tvDes.text = intent.getStringExtra("Title")
-        tvTit.text = intent.getStringExtra("Description")
-    }
-
-    private fun deleteRecord(id: String) {
-        val dbRef = FirebaseDatabase.getInstance().getReference("Control").child(id)
-        val mTask = dbRef.removeValue()
-
-        mTask.addOnSuccessListener {
-            Toast.makeText(this, "deleted", Toast.LENGTH_LONG).show()
-            val intent = Intent(this, MainActivity::class.java)
-            finish()
-            startActivity(intent)
-        }.addOnFailureListener { error ->
-            Toast.makeText(this, "Deleting Error ${error.message}", Toast.LENGTH_LONG).show()
-        }
-    }
-
 }
