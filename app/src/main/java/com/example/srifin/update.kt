7b96fd5383e6d01c12dca13a0b3a88726b1b6aa1
Sadeesha Ashan <120770131+Sadeeshaashan@users.noreply.github.com>
2023-05-14 21:54:54 +0530

    Updated

--- app/src/main/java/com/example/srifin/update.kt
+++ app/src/main/java/com/example/srifin/update.kt
@@ -1,40 +1,67 @@
 package com.example.srifin
 
-
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import androidx.appcompat.app.AppCompatActivity
-import com.example.srifin.R
+import android.widget.Toast
 import com.google.firebase.database.FirebaseDatabase
 
-class update : AppCompatActivity() {
-
+class Update : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update)
 
-        val titleEditText = findViewById<EditText>(R.id.Title)
-        val descriptionEditText = findViewById<EditText>(R.id.Des)
-        val updateButton = findViewById<Button>(R.id.btnUpData)
 
-        updateButton.setOnClickListener {
-            // Get the values of the title and description fields
-            val title = titleEditText.text.toString()
-            val description = descriptionEditText.text.toString()
+        val categoryname = intent.getStringExtra("categoryname")
+        val categorydescription = intent.getStringExtra("categorydescription")
+        val collectionid = intent.getStringExtra("collectionid")
 
-            // Update the values in the Firebase database
-            val database = FirebaseDatabase.getInstance().reference
-            val updatesRef = database.child("updates")
-            val newUpdateRef = updatesRef.push()
-            val update = Update(title, description)
-            newUpdateRef.setValue(update)
 
-            // Clear the fields
-            titleEditText.text.clear()
-            descriptionEditText.text.clear()
+        val descriptionEditText = findViewById<EditText>(R.id.updatecatDescription)
+        val nameEditText = findViewById<EditText>(R.id.updatecatName)
+        val updateButton = findViewById<Button>(R.id.updatebtn)
+        val deleteButton = findViewById<Button>(R.id.deletebtn)
+
+
+        nameEditText.hint = categoryname
+        descriptionEditText.hint = categorydescription
+
+
+
+        //delete
+
+        deleteButton.setOnClickListener {
+            val categoryRef = collectionid?.let { it1 ->
+                FirebaseDatabase.getInstance().getReference("categories").child(
+                    it1
+                )
+            }
+            if (categoryRef != null) {
+                categoryRef.removeValue()
+            }
+            Toast.makeText(this, "Category collection deleted successfully!", Toast.LENGTH_SHORT).show()
+            finish()
         }
-    }
 
-    data class Update(val title: String = "", val description: String = "")
+        //update
+        updateButton.setOnClickListener {
+            val categoryRef = collectionid?.let { it1 ->
+                FirebaseDatabase.getInstance().getReference("categories").child(
+                    it1
+                )
+            }
+            val newName = nameEditText.text.toString()
+            val newDescription = descriptionEditText.text.toString()
+            val updatedCategory = mapOf("categoryname" to newName, "categorydescription" to newDescription)
+            if (categoryRef != null) {
+                categoryRef.setValue(updatedCategory)
+            }
+            Toast.makeText(this, "Category collection updated successfully!", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+
+
+    }
 }
